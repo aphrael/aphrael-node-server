42cfb15526830defe4a2c1a3f99bb3f65c47c839
mapserver2007 <mapserver2007@gmail.com>
2013-07-12 16:07:09 +0900

    エラー処理を追加

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /**
  * server.js
- * version: 0.0.1 (2013/07/05)
+ * version: 0.0.2 (2013/07/12)
  *
  * Licensed under the MIT:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -8,16 +8,19 @@
  * Copyright 2013, Ryuichi TANAKA [mapserver2007@gmail.com]
  */
 
+var WS_PORT = 9224;
+var REST_PORT = 9225;
+
 // Logger
 var log4js = require('log4js');
 log4js.configure('./log4js.json');
 var logger = log4js.getLogger('dataFile');
+logger.setLevel('INFO');
 
 // WebSocket
-var WebSocketServer = require('ws').Server,
-    httpServer = require('http').createServer();
+var WebSocketServer = require('ws').Server;
 var server = new WebSocketServer({
-    server: httpServer
+    port: WS_PORT
 });
 
 var connection = null;
@@ -27,8 +30,6 @@ server.on('connection', function(ws) {
         logger.info("connecton closed: CODE [" + code + "]");
     });
 });
-
-httpServer.listen("9222");
 
 // REST API
 var express = require('express');
@@ -43,9 +44,15 @@ app.get('/rest/position', function(req, res) {
         res.send(200);
     }
     catch (e) {
-        logger.error(e.message);
-        throw e;
+        // Chromeからのコネクションがない場合
+        if (/Cannot call method 'send'/.test(e.message)) {
+            logger.info("WebSocket connection is required from Chrome.");
+            res.send(404);
+        }
+        else {
+            logger.error(e.stack);
+            throw e;
+        }
     }
-
 });
-app.listen("9223");
+app.listen(REST_PORT);
